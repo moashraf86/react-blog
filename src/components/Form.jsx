84aaf26d067ddf3fdd37b6745e3e05bd25e43ad9
Mohamed Ashraf <botoashraf123@gmail.com>
2024-05-25 19:11:01 +0300

    add from validation

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,21 +1,16 @@
 /* eslint-disable react/prop-types */
 export const Form = ({
-  onsubmit,
   heading,
   title,
-  setTitle,
   content,
-  setContent,
-  setTag,
   tag,
+  image,
+  onsubmit,
   handleImageChange,
   handleRemoveImage,
-  image,
-  validated,
+  handleChange,
+  errors,
 }) => {
-  const inValidTitle = validated && !title;
-  const inValidContent = validated && !content;
-  const inValidTag = validated && !tag;
   return (
     <div className="flex justify-center items-center max-w-[400px] mx-auto">
       <div className="flex flex-col w-full bg-zinc-900 border border-zinc-800 rounded-md p-6 gap-4 mt-6">
@@ -27,54 +22,60 @@ export const Form = ({
             <input
               name="title"
               id="title"
-              className={`w-full p-2 border text-zinc-50 bg-zinc-900 rounded-md ${
-                inValidTitle ? "border-red-600" : "border-zinc-800"
+              className={`w-full p-2 border text-zinc-50 border-zinc-800 bg-zinc-900 rounded-md ${
+                errors.title && "border-red-600"
               }`}
               value={title}
               type="text"
               placeholder="Add title"
-              onChange={(e) => setTitle(e.target.value)}
+              onChange={(e) => handleChange(e)}
             />
-            <p className="text-sm text-red-600">
-              {inValidTitle && "title is required"}
-            </p>
+            {errors.title && (
+              <p className="text-sm text-red-600">{errors.title}</p>
+            )}
           </div>
 
           <div className="flex flex-col gap-1">
             <textarea
               rows={4}
-              className={`w-full p-2 text-zinc-50 border bg-zinc-900 rounded-md ${
-                inValidContent ? "border-red-600" : "border-zinc-800"
+              className={`w-full p-2 text-zinc-50 border border-zinc-800 bg-zinc-900 rounded-md ${
+                errors.content && "border-red-600"
               }`}
               placeholder="write something here..."
               value={content}
-              onChange={(e) => setContent(e.target.value)}
+              name="content"
+              onChange={(e) => handleChange(e)}
             ></textarea>
-            <p className="text-sm text-red-600">
-              {inValidContent && "content is required"}
-            </p>
+            {errors.content && (
+              <p className="text-sm text-red-600">{errors.content}</p>
+            )}
           </div>
           {!image && (
-            <label
-              tabIndex="0"
-              htmlFor="image"
-              className="p-2 border border-dashed border-zinc-800 min-h-20 flex items-center justify-center rounded-md cursor-pointer bg-zinc-900 text-zinc-200 focus:outline-none focus:ring-2 focus:ring-zinc-50 "
-              onKeyDown={(e) => {
-                if (e.key === "Enter") {
-                  e.preventDefault();
-                  document.getElementById("image").click();
-                }
-              }}
-            >
-              click to upload image
-              <input
-                id="image"
-                type="file"
-                accept="image/*"
-                onChange={handleImageChange}
-                hidden
-              />
-            </label>
+            <>
+              <label
+                tabIndex="0"
+                htmlFor="image"
+                className="p-2 border border-dashed border-zinc-800 min-h-20 flex items-center justify-center rounded-md cursor-pointer bg-zinc-900 text-zinc-200 focus:outline-none focus:ring-2 focus:ring-zinc-50 "
+                onKeyDown={(e) => {
+                  if (e.key === "Enter") {
+                    e.preventDefault();
+                    document.getElementById("image").click();
+                  }
+                }}
+              >
+                click to upload image
+                <input
+                  id="image"
+                  type="file"
+                  accept="image/*"
+                  onChange={handleImageChange}
+                  hidden
+                />
+              </label>
+              {errors.image && (
+                <p className="text-sm text-red-600">{errors.image}</p>
+              )}
+            </>
           )}
           {image && (
             <div className="relative rounded-md overflow-clip">
@@ -115,58 +116,56 @@ export const Form = ({
               />
             </div>
           )}
-          <div className="flex gap-3 items-center">
+          <div className="flex flex-wrap gap-3 items-center">
             <label
               htmlFor="tech"
-              className={`text-zinc-50 py-1 px-4 border rounded-full cursor-pointer has-[:checked]:bg-zinc-600 ${
-                inValidTag ? "border-red-600" : "border-zinc-800"
-              }`}
+              className={`text-zinc-50 py-1 px-4 border border-zinc-800 rounded-full cursor-pointer has-[:checked]:bg-zinc-600`}
             >
               <input
                 type="radio"
                 id="tech"
                 value="tech"
-                name="postTag"
+                name="tag"
                 className="text-zinc-50"
                 hidden
-                onChange={(e) => setTag(e.target.value)}
+                checked={tag === "tech"}
+                onChange={(e) => handleChange(e)}
               />
               Tech
             </label>
             <label
               htmlFor="culture"
-              className={`text-zinc-50 py-1 px-4 border rounded-full cursor-pointer has-[:checked]:bg-zinc-600 ${
-                inValidTag ? "border-red-600" : "border-zinc-800"
-              }`}
+              className={`text-zinc-50 py-1 px-4 border border-zinc-800 rounded-full cursor-pointer has-[:checked]:bg-zinc-600 `}
             >
               <input
                 type="radio"
                 id="culture"
                 value="culture"
-                name="postTag"
+                name="tag"
                 className="text-zinc-50"
                 hidden
-                onChange={(e) => setTag(e.target.value)}
+                checked={tag === "culture"}
+                onChange={(e) => handleChange(e)}
               />
               Culture
             </label>
             <label
               htmlFor="science"
-              className={`text-zinc-50 py-1 px-4 border rounded-full cursor-pointer has-[:checked]:bg-zinc-600 ${
-                inValidTag ? "border-red-600" : "border-zinc-800"
-              }`}
+              className={`text-zinc-50 py-1 px-4 border border-zinc-800 rounded-full cursor-pointer has-[:checked]:bg-zinc-600`}
             >
               <input
                 type="radio"
                 id="science"
                 value="science"
-                name="postTag"
+                name="tag"
                 className="text-zinc-50"
                 hidden
-                onChange={(e) => setTag(e.target.value)}
+                checked={tag === "science"}
+                onChange={(e) => handleChange(e)}
               />
               Science
             </label>
+            {errors.tag && <p className="text-sm text-red-600">{errors.tag}</p>}
           </div>
           <button
             type="submit"
