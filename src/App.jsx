2a7488fcb50f4c56ec2400bf673dfce2c388bf8f
Mohamed Ashraf <botoashraf123@gmail.com>
2024-06-15 00:59:59 +0300

    refactor: Add ThemeProvider for managing app theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
 import { Outlet } from "react-router-dom";
-import Header from "./components/Header";
+import Header from "./pages/Header";
 import { PostsProvider } from "./context/PostsContext";
 import { AuthProvider } from "./context/AuthContext";
+import { ThemeProvider } from "./context/ThemeProviderContext";
 
 export default function App() {
   return (
-    <PostsProvider>
-      <AuthProvider>
-        <div className="container h-full mx-auto px-4">
-          <Header />
-          <Outlet />
-        </div>
-      </AuthProvider>
-    </PostsProvider>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <PostsProvider>
+        <AuthProvider>
+          <div className="container h-full mx-auto px-4">
+            <Header />
+            <Outlet />
+          </div>
+        </AuthProvider>
+      </PostsProvider>
+    </ThemeProvider>
   );
 }
