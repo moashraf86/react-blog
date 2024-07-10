d38c65c478b20822e0a1ac830fa4ffc3c89ad334
Mohamed Ashraf <botoashraf123@gmail.com>
2024-07-10 20:40:59 +0300

    refactor: Convert Filter component to use shadcn-ui select component

--- src/components/shared/Filter.jsx
+++ src/components/shared/Filter.jsx
@@ -1,62 +1,26 @@
+/* eslint-disable react/prop-types */
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "../ui/select";
 export const Filter = ({ handleFilter }) => {
   return (
-    <div className="flex items-center gap-3  text-zinc-400">
-      <label
-        htmlFor="all"
-        className="hover:text-primary cursor-pointer text-sm has-[:checked]:text-primary"
-      >
-        <input
-          type="radio"
-          name="filter"
-          id="all"
-          className="hidden"
-          value="all"
-          onChange={(e) => handleFilter(e.target.value)}
-        />
-        All
-      </label>
-      <label
-        htmlFor="tech"
-        className="hover:text-primary cursor-pointer text-sm has-[:checked]:text-primary"
-      >
-        <input
-          type="radio"
-          name="filter"
-          id="tech"
-          className="hidden"
-          value="tech"
-          onChange={(e) => handleFilter(e.target.value)}
-        />
-        Tech
-      </label>
-      <label
-        htmlFor="science"
-        className="hover:text-primary cursor-pointer text-sm has-[:checked]:text-primary"
-      >
-        <input
-          type="radio"
-          name="filter"
-          id="science"
-          className="hidden"
-          value="science"
-          onChange={(e) => handleFilter(e.target.value)}
-        />
-        Science
-      </label>
-      <label
-        htmlFor="culture"
-        className="hover:text-primary cursor-pointer text-sm has-[:checked]:text-primary"
-      >
-        <input
-          type="radio"
-          name="filter"
-          id="culture"
-          className="hidden"
-          value="culture"
-          onChange={(e) => handleFilter(e.target.value)}
-        />
-        Culture
-      </label>
-    </div>
+    <Select onValueChange={(value) => handleFilter(value)}>
+      <SelectTrigger className="w-[180px]">
+        <SelectValue placeholder="Filter" />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectGroup>
+          <SelectItem value="all">All</SelectItem>
+          <SelectItem value="tech">Tech</SelectItem>
+          <SelectItem value="science">Science</SelectItem>
+          <SelectItem value="culture">Culture</SelectItem>
+        </SelectGroup>
+      </SelectContent>
+    </Select>
   );
 };
