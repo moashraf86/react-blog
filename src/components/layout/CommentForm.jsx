d079cf11fed84f998d12889d75d9b504e3d0b87c
Mohamed Ashraf <botoashraf123@gmail.com>
2024-07-17 04:06:13 +0300

    refactor: Update CommentForm to improve accessibility and handle guest users

--- src/components/layout/CommentForm.jsx
+++ src/components/layout/CommentForm.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
 import { Button } from "../ui/button";
 export const CommentForm = ({
@@ -9,15 +8,7 @@ export const CommentForm = ({
   error,
   formRef,
 }) => {
-  const userImg =
-    currentUser?.photoURL || "https://robohash.org/mail@ashallendesign.co.uk";
   const isGuest = currentUser?.isGuest;
-
-  //view the form only if the user is not a guest
-  if (!currentUser || isGuest) {
-    return;
-  }
-
   return (
     <div ref={formRef}>
       <label
@@ -28,17 +19,24 @@ export const CommentForm = ({
       </label>
       <form onSubmit={handleSubmit} className="flex flex-col gap-3 flex-1">
         <textarea
-          className="w-full p-3 text-primary border border-input bg-transparent rounded-md shadow-sm focus:ring-0 focus:border-primary"
+          className="w-full p-3 text-primary border border-input bg-transparent rounded-md shadow-sm focus:ring-0 focus:border-primary disabled:cursor-not-allowed disabled:opacity-50"
           name="comment"
           id="comment"
           rows={4}
-          placeholder="write comment"
+          placeholder={
+            isGuest || !currentUser ? "Sign in to comment" : "Write a comment"
+          }
           onChange={handleChangeComment}
           value={content}
+          disabled={isGuest || !currentUser}
         ></textarea>
-
         {error && <p className="text-danger">{error}</p>}
-        <Button size="lg" type="submit" className="self-end text-base">
+        <Button
+          size="lg"
+          type="submit"
+          className="self-end text-base"
+          disabled={isGuest || !currentUser}
+        >
           Send
         </Button>
       </form>
