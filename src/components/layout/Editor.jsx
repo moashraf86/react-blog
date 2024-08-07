d3b58b29308b12878e235d66190c3f876e1ee69d
Mohamed Ashraf <botoashraf123@gmail.com>
2024-08-07 23:08:06 +0300

    feat: create MDEditor component

--- src/components/layout/Editor.jsx
+++ src/components/layout/Editor.jsx
@@ -1,10 +1,41 @@
 /* eslint-disable react/prop-types */
 import MDEditor from "@uiw/react-md-editor";
+import { markdownToPlainText } from "../../utils/markdownToPlainText";
+import { useState } from "react";
+import { debounce } from "../../utils/debounce";
+import { handleToolbarIcons } from "../../utils/handleToolbarIcons";
 
 export const Editor = ({ value, onChange }) => {
+  /**
+   * Handle character count
+   */
+  const [count, setCount] = useState(0);
+  const handleCount = debounce((content) => {
+    const plainText = markdownToPlainText(content);
+    const count = plainText.length;
+    setCount(count);
+  }, 300);
+
   return (
-    <div className="container">
-      <MDEditor value={value} onChange={onChange} />
-    </div>
+    <>
+      <MDEditor
+        height="auto"
+        preview="edit"
+        value={value}
+        onChange={onChange}
+        onInput={(e) => {
+          handleCount(e.target.value);
+          e.target.style.height = "auto";
+          e.target.style.height = e.target.scrollHeight + "px";
+        }}
+        className="markdown-editor"
+        commandsFilter={handleToolbarIcons}
+        textareaProps={{
+          placeholder: "Write your post content here...",
+        }}
+        visibleDragbar={false}
+      />
+      <p className="flex justify-end text-muted-foreground">{count}/10000</p>
+    </>
   );
 };
