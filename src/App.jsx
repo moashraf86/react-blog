63991b7b3f3d7a74ec62f50d6545b4e31f5c9967
Mohamed Ashraf <botoashraf123@gmail.com>
2024-08-09 17:10:36 +0300

    feat: wrap QueryProvider around app

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,20 @@ import { PostsProvider } from "./context/PostsContext";
 import { AuthProvider } from "./context/AuthContext";
 import { ThemeProvider } from "./context/ThemeProviderContext";
 import { CommentsProvider } from "./context/CommentsContext";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+const queryClient = new QueryClient();
 export default function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <PostsProvider>
         <AuthProvider>
           <Header />
-          <CommentsProvider>
-            <Outlet />
-          </CommentsProvider>
+          <QueryClientProvider client={queryClient}>
+            <CommentsProvider>
+              <Outlet />
+            </CommentsProvider>
+          </QueryClientProvider>
         </AuthProvider>
       </PostsProvider>
     </ThemeProvider>
